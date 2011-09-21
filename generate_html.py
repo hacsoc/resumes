768f8763c0cc6ace9e5ec18cbfaa441e4e0ab7a7
Stephen Johnson <srj15@case.edu>
2011-09-21 10:46:57 -0400

    Comments

--- generate_html.py
+++ generate_html.py
@@ -24,20 +24,27 @@ with open('data.csv') as csv_f, open('index.html', 'w') as html_f:
     tag_map = {p.name: [t.strip().lower() for t in p.tags.split(',')]
                for p in people}
 
+
+    # start generating HTML
     template.write_header(html_f)
 
+    # generate the sidebar, putting some space between tag groups
     for tag_list in template.tag_lists:
         for tag in tag_list:
             template.write_tag(html_f, tag)
-        html_f.write('<br/><hr/>')
+        template.write_page_divider(html_f)
 
+    # move to the main content area
     template.write_separator(html_f)
 
+    # list each person's name, email, tags, major, resume link
     for person in people:
         template.write_person(html_f, person)
 
+    # move on to tag list
     template.write_page_divider(html_f)
 
+    # for each tag, show links to all the people under it
     all_tags = sorted(set(itertools.chain(*tag_map.values())))
     for tag in all_tags:
         template.write_tag_header(html_f, tag)
@@ -45,4 +52,5 @@ with open('data.csv') as csv_f, open('index.html', 'w') as html_f:
             if tag in tag_map[person.name]:
                 template.write_person_link(html_f, person)
     
+    # all done!
     template.write_footer(html_f)
