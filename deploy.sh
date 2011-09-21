a8d9584c11796e8c91434956da45654324725b9f
Stephen Johnson <srj15@case.edu>
2011-09-21 11:02:33 -0400

    Fixed deploy script params

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ git checkout gh-pages &&
 rm index.html &&
 mv /tmp/index.html index.html &&
 git add index.html &&
-git commit -m $1 &&
+git commit -m $@ &&
 git push origin gh-pages &&
 git checkout master
